513478a337b86408652e367d78f000deaf906928
Talha Khan <TalhaKhan@MacBook-Pro.local>
2023-05-31 16:24:28 +0500

    Side Nav

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -1,18 +1,74 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
-
-const items = [
+let data = [
   {
-    title: "Get started",
-    links: [
+    heading: "Tax Heading",
+    collapsed: false,
+    data: [
       {
-        href: "/docs",
-        children: "Docs",
+        title: "Tax Calculations",
+        selected: true,
+        links: [
+          {
+            href: "/docs",
+            children: "The calculation object",
+          },
+          {
+            href: "/books",
+            children: "Calculate tax",
+          },
+        ],
       },
       {
-        href: "/books",
-        children: "Books",
+        title: "Tax Transactions",
+        selected: false,
+
+        links: [
+          {
+            href: "/docs",
+            children: "The transaction object",
+          },
+          {
+            href: "/books",
+            children: "Retrieve a transaction",
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    heading: "Tax",
+    collapsed: true,
+    data: [
+      {
+        title: "Tax Calculations",
+        selected: false,
+        links: [
+          {
+            href: "/docs",
+            children: "The calculation object",
+          },
+          {
+            href: "/books",
+            children: "Calculate tax",
+          },
+        ],
+      },
+      {
+        title: "Tax Transactions",
+        selected: false,
+        links: [
+          {
+            href: "/docs",
+            children: "The transaction object",
+          },
+          {
+            href: "/books",
+            children: "Retrieve a transaction",
+          },
+        ],
       },
     ],
   },
@@ -20,22 +76,73 @@ const items = [
 
 export function SideNav() {
   const router = useRouter();
+  let [items, setItems] = useState([]);
+
+  useEffect(() => {
+    setItems(data);
+    console.log("ss");
+  }, []);
 
   return (
     <nav className="sidenav">
-      {items.map((item) => (
-        <div key={item.title}>
-          <span>{item.title}</span>
-          <ul className="flex column">
-            {item.links.map((link) => {
-              const active = router.pathname === link.href;
-              return (
-                <li key={link.href} className={active ? "active" : ""}>
-                  <Link {...link} />
-                </li>
-              );
-            })}
-          </ul>
+      {items.map((item, index) => (
+        <div key={item.heading}>
+          <span
+            onClick={(event) => {
+              console.log("index");
+
+              items[index].collapsed = !items[index].collapsed;
+              let data = items;
+              setItems((items) => [...data]);
+              // setItems(items);
+            }}
+          >
+            {item.heading}
+          </span>
+
+          {item.collapsed ? (
+            <></>
+          ) : (
+            item.data.map((data, child) => (
+              <div key={data.title}>
+                <h5
+                  onClick={(event) => {
+                    items[index].data[child].selected =
+                      !items[index].data[child].selected;
+                    for (const i of item.data) {
+                    }
+
+                    for (let i = 0; i < item.data.length; i++) {
+                      console.log(i + "sdasd");
+                      if (i != child) {
+                        items[index].data[i].selected = false;
+                      }
+                    }
+                    let data = items;
+                    setItems((items) => [...data]);
+                    // setItems(items);
+                  }}
+                >
+                  {data.title}
+                </h5>
+
+                <ul className="flex column">
+                  {items[index].data[child].selected ? (
+                    data.links.map((link) => {
+                      const active = router.pathname === link.href;
+                      return (
+                        <li key={link.href} className={active ? "active" : ""}>
+                          <Link {...link} />
+                        </li>
+                      );
+                    })
+                  ) : (
+                    <></>
+                  )}
+                </ul>
+              </div>
+            ))
+          )}
         </div>
       ))}
       <style jsx>
